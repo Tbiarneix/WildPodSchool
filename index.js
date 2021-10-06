db3d282633fa260e682e20686bea66cfd760dc90
Malik Cherfi <malikcerfi@hotmail.fr>
2021-10-06 15:54:37 +0200

    ajout liste p contact pour modifier langue

--- index.js
+++ index.js
@@ -1,34 +1,54 @@
-const button = document.querySelector(".test");
-const contactForm = document.querySelector(".contactForm")
-const contactForm2 = document.querySelector(".contactForm2")
+const contactFormFrench = document.querySelector(".formu-french");
+const contactFormStarWars = document.querySelector(".formu-star-wars");
+const contactFormClingon = document.querySelector(".formu-clingon");
+
+
+const clingon = document.querySelector(".clingon")
+const starWars = document.querySelector(".starwars")
+const french = document.querySelector(".french")
+
+const labelFrench = document.querySelector(".label-french")
+const labelStarWars = document.querySelector(".label-star-wars")
+const labelClingon = document.querySelector(".label-clingon")
 
 
 
-if(contactForm.style.display = "block") {
-    button.onclick =  function() {
-             contactForm.style.display = "none";
-        
-            contactForm2.style.display = "block";
-    }
-}
-else {
-    button.onclick =  function() {
-        contactForm.style.display = "block";
-   
-       contactForm2.style.display = "none";
-}
-}
+clingon.addEventListener("click", function () {
+    contactFormFrench.style.display = "none";
+    contactFormStarWars.style.display = "none";
+    contactFormClingon.style.display = "block";
+
+    labelFrench.style.display = "none";
+    labelStarWars.style.display = "none";
+    labelClingon.style.display = "block";
+})
 
 
+starWars.addEventListener("click", function () {
+    contactFormFrench.style.display = "none";
+    contactFormClingon.style.display = "none";
+    contactFormStarWars.style.display = "block";
+
+    labelFrench.style.display = "none";
+    labelClingon.style.display = "none";
+    labelStarWars.style.display = "block";
+})
+
+french.addEventListener("click", function () {
+    contactFormStarWars.style.display = "none";
+    contactFormClingon.style.display = "none";
+    contactFormFrench.style.display = "block";
+
+    labelStarWars.style.display = "none";
+    labelClingon.style.display = "none";
+    labelFrench.style.display = "block";
+})
 
 
-// Si contactForm est en block le mettre en none et contactForm2 en block
-//Sinon le mettre en block et contactForm2 en none
-// repeter ces condition a l'infini
 const menuBurger = document.querySelector('.hamburger');
 const menuMobile = document.querySelector('.menu-mobile');
 
-menuBurger.addEventListener('click', function() { 
+menuBurger.addEventListener('click', function () {
     menuBurger.classList.toggle('is-active');
     menuMobile.classList.toggle('is-active');
 });

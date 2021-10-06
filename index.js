bcece9c1c89ef7db2fcbf6e5ca6c00d2e80428fa
Malik Cherfi <malikcerfi@hotmail.fr>
2021-10-06 10:20:57 +0200

    ajout fonctionnalité js

---
+++ index.js
@@ -0,0 +1,27 @@
+const button = document.querySelector(".test");
+const contactForm = document.querySelector(".contactForm")
+const contactForm2 = document.querySelector(".contactForm2")
+
+
+
+if(contactForm.style.display = "block") {
+    button.onclick =  function() {
+             contactForm.style.display = "none";
+        
+            contactForm2.style.display = "block";
+    }
+}
+else {
+    button.onclick =  function() {
+        contactForm.style.display = "block";
+   
+       contactForm2.style.display = "none";
+}
+}
+
+
+
+
+// Si contactForm est en block le mettre en none et contactForm2 en block
+//Sinon le mettre en block et contactForm2 en none
+// repeter ces condition a l'infini

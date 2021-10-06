694dc2018d92309a7f9c8cbeca9b458101bdf565
Emma <emma.nouti@gmail.com>
2021-10-06 16:04:01 +0200

    add generateur de citations + sound effect menu

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const menuBurger = document.querySelector('.hamburger');
 const menuMobile = document.querySelector('.menu-mobile');
 
-menuBurger.addEventListener('click', function() { 
-    menuBurger.classList.toggle('is-active');
-    menuMobile.classList.toggle('is-active');
-});
+// menuBurger.addEventListener('click', function() { 
+// menuBurger.classList.toggle('is-active');
+// menuMobile.classList.toggle('is-active');
+// });
+
+
+
+const menuNav = document.querySelectorAll('nav li');
+var audiosaber = document.querySelector("#lightsaber");
+for (let i=0; i<menuNav.length; i++) {
+    menuNav[i].addEventListener('mouseover', function() {
+        audiosaber.play();
+    })
+}

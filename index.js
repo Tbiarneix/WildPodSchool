3b40f6b7f53805e57ec49f528348078bf12f1329
Malik Cherfi <malikcerfi@hotmail.fr>
2021-10-06 20:09:17 +0200

    modification media query page contact

--- index.js
+++ index.js
@@ -49,10 +49,10 @@ french.addEventListener("click", function () {
 const menuBurger = document.querySelector('.hamburger');
 const menuMobile = document.querySelector('.menu-mobile');
 
-// menuBurger.addEventListener('click', function() { 
-// menuBurger.classList.toggle('is-active');
-// menuMobile.classList.toggle('is-active');
-// });
+ menuBurger.addEventListener('click', function() { 
+ menuBurger.classList.toggle('is-active');
+ menuMobile.classList.toggle('is-active');
+ });
 
 
 

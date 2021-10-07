4580b4b1c168e955b7ebddc6ec36f64619801412
Tbiarneix <thomas.biarneix@gmail.com>
2021-10-07 17:19:06 +0200

    Ajout de la page Team + css + js

--- team.js
+++ team.js
@@ -1,11 +1,35 @@
-const modalButton = document.querySelector('#open-modal')
+const modalButton = document.querySelectorAll('.open-modal');
 const modal = document.querySelector('.modal');
 const closeModalButton = document.querySelector('#close-modal');
 
-modalButton.addEventListener("click", function() {
-    modal.style.display = "block";
-})
+const modalSelection = function () {
+    for (let i = 0; i < modalButton.length; i++) {
+        modalButton[i].addEventListener("click", function () {
+            modal.style.display = "block";
+        })
+    }
+}
 
-closeModalButton.addEventListener("click", function() {
+modalSelection();
+closeModalButton.addEventListener("click", function () {
     modal.style.display = "none";
 })
+
+
+// const prevArrow = document.querySelector('#prev-arrow');
+// const nextArrow = document.querySelector('#next-arrow');
+// const cardsNav = document.getElementsByClassName('carousel-item');
+// let selectedId = 3;
+
+
+// prevArrow.addEventListener("click", function () {
+//     selectedId -= 1;
+//     prevArrow.setAttribute("href", `#formator${selectedId}`);
+// })
+
+// nextArrow.addEventListener("click", function () {
+//     selectedId += 1;
+//     nextArrow.setAttribute("href", `#formator${selectedId}`);
+// })
+
+
